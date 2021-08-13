9dd698b9e0322fbbc73e57ee389a92aa7b26bbb1
Enes <enesrufgurel@gmail.com>
2021-08-13 23:13:05 +0300

    Add project files.

---
+++ MyDictionary/MyDictonary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictonary<TKey, TValue>
+    {
+        TKey[] key;
+        TValue[] value;
+
+        public MyDictonary()
+        {
+            key = new TKey[0];
+            value = new TValue[0];
+        }
+        public void Add(TKey item1, TValue item2)
+        {
+            TKey[] tempArrayKey = key;
+            TValue[] tempArrayValue = value;
+
+            key = new TKey[key.Length + 1];
+            value = new TValue[value.Length + 1];
+
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                key[i] = tempArrayKey[i];
+                value[i] = tempArrayValue[i];
+            }
+
+            key[key.Length - 1] = item1;
+            value[value.Length - 1] = item2;
+
+            Console.WriteLine(item1 + " numaralı " + item2 + " adlı kullanıcı sisteme başarıyla eklendi!");
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<int, string> myDictonary1 = new MyDictonary<int, string>();
+            myDictonary1.Add(1, "Engin");
+
+            MyDictonary<int, string> myDictonary2 = new MyDictonary<int, string>();
+            myDictonary2.Add(2, "Enes");
+        }
+    }
+}
